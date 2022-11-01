2c2b07c2d4190492d8755905b70b0aa87708ef5d
Vitor Scheffer <vitorscheffer61@gmail.com>
2022-11-01 17:52:29 -0300

    Ajustando texto botao chamar e cancelar uber mudando style do botao (background).

--- UberClone/ViewController/PassageiroViewController.swift
+++ UberClone/ViewController/PassageiroViewController.swift
@@ -12,7 +12,10 @@ import MapKit
 
 class PassageiroViewController: UIViewController, CLLocationManagerDelegate {
     
+    @IBOutlet weak var botaoChamar: UIButton!
     @IBOutlet weak var mapa: MKMapView!
+    var uberChamado = false
+    
     var gerenciadorLocalizacao = CLLocationManager()
     var localUsuario = CLLocationCoordinate2D()
     
@@ -30,16 +33,36 @@ class PassageiroViewController: UIViewController, CLLocationManagerDelegate {
         let requisicao = database.child("requisicoes")
         let autenticacao = Auth.auth()
         
+        
         if let emailUsuario = autenticacao.currentUser?.email {
-            let dadosUsuario = [
-                "email": emailUsuario,
-                "nome":"Vitor Passageiro",
-                "latitude" : self.localUsuario.latitude,
-                "longitude" : self.localUsuario.longitude
-            ] as [String : Any]
-            requisicao.childByAutoId().setValue( dadosUsuario )
+            if uberChamado == true {
+                self.alternaBotaoChamar()
+            } else {
+                self.alternaBotaoCancelar()
+                
+                let dadosUsuario = [
+                    "email": emailUsuario,
+                    "nome":"Vitor Passageiro",
+                    "latitude" : self.localUsuario.latitude,
+                    "longitude" : self.localUsuario.longitude
+                ] as [String : Any]
+                requisicao.childByAutoId().setValue( dadosUsuario )
+            }
+            
         }
         
+    }
+    
+    func alternaBotaoChamar() {
+        self.botaoChamar.setTitle("Chamar Uber", for: .normal)
+        self.botaoChamar.backgroundColor = UIColor(displayP3Red: 0.067, green: 0.576, blue: 0.604, alpha: 1)
+        self.uberChamado = false
+    }
+    
+    func alternaBotaoCancelar() {
+        self.botaoChamar.backgroundColor = UIColor(displayP3Red: 0.831, green: 0.237, blue: 0.146, alpha: 1)
+        self.botaoChamar.setTitle("Cancelar Uber", for: .normal)
+        self.uberChamado = true
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
